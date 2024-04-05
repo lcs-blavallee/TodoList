2d6a45ab863a58fc6f582843f4682dae95247654
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-04-05 09:16:19 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Benjamin Lavallee on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
