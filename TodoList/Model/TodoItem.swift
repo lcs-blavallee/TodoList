22cdc76e998e7e3fa7fab019c916207c26ac4eb2
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-04-09 18:47:09 -0400

    Adjusted model for a to-do item to use the SwiftData framework.

--- TodoList/Model/TodoItem.swift
+++ TodoList/Model/TodoItem.swift
@@ -5,17 +5,23 @@
 //  Created by Benjamin Lavallee on 2024-04-05.
 //
 
-import Foundation
+import SwiftData
 
-struct TodoItem: Identifiable {
-    let id = UUID()
+@Model
+class TodoItem: Identifiable {
     var title: String
     var done: Bool
+    
+    init(title: String, done: Bool) {
+        self.title = title
+        self.done = done
+    }
 }
 
 let firstItem = TodoItem(title: "Study for Chemistry quiz", done: false)
 
 let secondItem = TodoItem(title: "Finish Computer Science assignment", done: true)
+
 let thirdItem = TodoItem(title: "Go for a run around campus", done: false)
 
 let exampleItems = [
